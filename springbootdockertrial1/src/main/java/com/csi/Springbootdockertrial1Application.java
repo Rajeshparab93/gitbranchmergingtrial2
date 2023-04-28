af76d1f8687064486a92cdb5958bcbfc4c192041
Your Namethoratswapnil061@gmail.com <you@example.com>
2023-04-28 14:59:00 +0530

    [STNO-2] Initial commit

---
+++ springbootdockertrial1/src/main/java/com/csi/Springbootdockertrial1Application.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdockertrial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootdockertrial1Application.class, args);
+	}
+
+}
---
+++ springbootdockertrial1/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,23 @@
+package com.csi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+
+    @GetMapping
+    public String sayHello(){
+        return "WELCOME TO FINTECH CSI PUNE";
+    }
+
+    @GetMapping("/services")
+    public ResponseEntity<String> sayServices(){
+        return ResponseEntity.ok("SOFTWARE DEVELOPMENT | MOBILE APP DEVELOPMENT");
+
+
+    }
+}
